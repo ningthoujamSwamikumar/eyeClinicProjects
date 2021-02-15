0f24854a4f706f65d1dd26b159f57b585844b5ef
nswamikumar <nswamikumar@gmail.com>
2021-02-15 14:18:23 +0530

    changing port variable to get the heroku port

--- app.js
+++ app.js
@@ -182,6 +182,10 @@ app.post("/dashboard/patient/foundClinical", (req,res)=>{
 });
 
 
-app.listen(3000, () => {
-  console.log("Server started at port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started on port: "+port);
 });
